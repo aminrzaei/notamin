26743ee19e334f836886333fb777ee763baeb24f
Amin Rezaei <my.aminrezaei@gmail.com>
2022-11-18 01:24:36 +0330

    feature: add tags and notes managementmodals

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
-import { Button, MultiSelect, TextInput } from "@mantine/core";
+import { useMemo, useState } from "react";
+import { Button, MultiSelect, TextInput, Modal } from "@mantine/core";
 import SearchIcon from "../assets/icons/SearchIcon";
 import { INoteWithTag } from "../reducers/notesReducer";
 import { ITag } from "../reducers/tagsReducer";
@@ -11,6 +11,8 @@ interface INavbarProps {
 }
 
 const Navbar = ({ tags, notes, setNotes }: INavbarProps) => {
+  const [isNotesModalOpen, setIsNotesModalOpen] = useState<boolean>(false);
+  const [isTagsModalOpen, setIsTagsModalOpen] = useState<boolean>(false);
   const tagForMultiselect = useMemo(() => {
     return tags.map((tag) => {
       return { value: tag.id, label: tag.title };
@@ -39,47 +41,67 @@ const Navbar = ({ tags, notes, setNotes }: INavbarProps) => {
   };
 
   return (
-    <div className="nav-container">
-      <TextInput
-        className="nav-container__search"
-        placeholder="Search note title "
-        rightSection={<SearchIcon color="#C1C2C5" />}
-        value={searchValue}
-        onChange={(e) => {
-          const searchTerm = e.target.value;
-          console.log(searchTerm);
-          setSearchValue(searchTerm);
-          handleSearch(searchTerm);
-        }}
-      />
-      <MultiSelect
-        className="nav-container__filter"
-        data={tagForMultiselect}
-        placeholder="Tags to see"
-        searchable
-        value={multiselectValue}
-        onChange={(selectedTags) => {
-          setMultiselectValue(selectedTags);
-          handleFilterByTags(selectedTags);
-        }}
-      />
-      <div className="nav-container__btn-container">
-        <Button
-          className="nav-container__btn--notes"
-          variant="light"
-          color="teal"
-        >
-          Manage Notes
-        </Button>
-        <Button
-          className="nav-container__btn--tags"
-          variant="light"
-          color="blue"
-        >
-          Manage Tags
-        </Button>
+    <>
+      <div className="nav-container">
+        <TextInput
+          className="nav-container__search"
+          placeholder="Search note title "
+          rightSection={<SearchIcon color="#C1C2C5" />}
+          value={searchValue}
+          onChange={(e) => {
+            const searchTerm = e.target.value;
+            console.log(searchTerm);
+            setSearchValue(searchTerm);
+            handleSearch(searchTerm);
+          }}
+        />
+        <MultiSelect
+          className="nav-container__filter"
+          data={tagForMultiselect}
+          placeholder="Tags to see"
+          searchable
+          value={multiselectValue}
+          onChange={(selectedTags) => {
+            setMultiselectValue(selectedTags);
+            handleFilterByTags(selectedTags);
+          }}
+        />
+        <div className="nav-container__btn-container">
+          <Button
+            className="nav-container__btn--notes"
+            variant="light"
+            color="teal"
+            onClick={() => setIsNotesModalOpen(true)}
+          >
+            Manage Notes
+          </Button>
+          <Button
+            className="nav-container__btn--tags"
+            variant="light"
+            color="blue"
+            onClick={() => setIsTagsModalOpen(true)}
+          >
+            Manage Tags
+          </Button>
+        </div>
       </div>
-    </div>
+      <Modal
+        opened={isTagsModalOpen}
+        onClose={() => setIsTagsModalOpen(false)}
+        centered
+        title="Tags"
+      >
+        Tags
+      </Modal>
+      <Modal
+        opened={isNotesModalOpen}
+        onClose={() => setIsNotesModalOpen(false)}
+        centered
+        title="Notes"
+      >
+        Notes
+      </Modal>
+    </>
   );
 };
 
--- src/pages/ShowNote.tsx
+++ src/pages/ShowNote.tsx
@@ -12,7 +12,7 @@ const ShowNote: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { noteId } = useParams();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { notes, tags } = useSelector((state: RootState) => state);
   const noteWithTags = useMemo(() => {
     const note = notes.find((note) => note.id === noteId);
@@ -21,74 +21,72 @@ const ShowNote: React.FC = () => {
     return { ...note, tags: noteTags };
   }, [notes, tags]);
   return (
-    <>
-      <div className="showpage">
-        <div className="showpage__header">
-          <h1 className="showpage__title">{noteWithTags.title}</h1>
-          <Link to="edit">
-            <ActionIcon
-              mt={10}
-              variant="outline"
-              color="indigo"
-              title="Edit this note"
-              mr={10}
-            >
-              <EditIcon color="#5c7cfa" />
-            </ActionIcon>
-          </Link>
+    <div className="showpage">
+      <div className="showpage__header">
+        <h1 className="showpage__title">{noteWithTags.title}</h1>
+        <Link to="edit">
           <ActionIcon
-            variant="filled"
-            color="red"
             mt={10}
-            ml="auto"
-            title="Delete this note"
-            onClick={() => setIsModalOpen(true)}
+            variant="outline"
+            color="indigo"
+            title="Edit this note"
+            mr={10}
           >
-            <DeleteIcon color="white" />
+            <EditIcon color="#5c7cfa" />
           </ActionIcon>
-        </div>
-        <div
-          className="showpage__body"
-          dangerouslySetInnerHTML={{ __html: noteWithTags.body || "" }}
-        />
-        <ul className="showpage__tag-container">
-          {noteWithTags.tags.map((tag) => {
-            return (
-              <li key={tag.id} className="showpage__tag">
-                {tag.title}
-              </li>
-            );
-          })}
-        </ul>
-        <div className="showpage__home-btn">
-          <Button color="blue" onClick={() => navigate("/", { replace: true })}>
-            Back to Home
-          </Button>
-        </div>
-        <Modal
-          opened={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          centered
-          title="Are you sure about deleting this note?"
+        </Link>
+        <ActionIcon
+          variant="filled"
+          color="red"
+          mt={10}
+          ml="auto"
+          title="Delete this note"
+          onClick={() => setIsModalOpen(true)}
         >
-          <Button
-            color="blue"
-            variant="subtle"
-            onClick={() => setIsModalOpen(false)}
-          >
-            Cancel
-          </Button>
-          <Button
-            color="red"
-            onClick={() => {
-              dispatch({ type: DELETE_NOTE, payload: noteWithTags.id });
-            }}
-          >
-            Delete this note
-          </Button>
-        </Modal>
+          <DeleteIcon color="white" />
+        </ActionIcon>
       </div>
-    </>
+      <div
+        className="showpage__body"
+        dangerouslySetInnerHTML={{ __html: noteWithTags.body || "" }}
+      />
+      <ul className="showpage__tag-container">
+        {noteWithTags.tags.map((tag) => {
+          return (
+            <li key={tag.id} className="showpage__tag">
+              {tag.title}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="showpage__home-btn">
+        <Button color="blue" onClick={() => navigate("/", { replace: true })}>
+          Back to Home
+        </Button>
+      </div>
+      <Modal
+        opened={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        centered
+        title="Are you sure about deleting this note?"
+      >
+        <Button
+          color="blue"
+          variant="subtle"
+          onClick={() => setIsModalOpen(false)}
+        >
+          Cancel
+        </Button>
+        <Button
+          color="red"
+          onClick={() => {
+            dispatch({ type: DELETE_NOTE, payload: noteWithTags.id });
+          }}
+        >
+          Delete this note
+        </Button>
+      </Modal>
+    </div>
   );
 };
 
